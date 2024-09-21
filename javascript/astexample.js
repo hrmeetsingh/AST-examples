5e6250615a7e36ccf8ffbaf15e61859b67428806
Harmeet Singh <er.harmeetsingh@hotmail.com>
2024-09-21 16:52:43 -0600

    Add javascript example

---
+++ javascript/astexample.js
@@ -0,0 +1,27 @@
+const acorn = require('acorn');
+const walk = require('acorn-walk');
+const escodegen = require('escodegen');
+
+const originalCode = `
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+greet("World");
+`;
+
+const ast = acorn.parse(originalCode, { ecmaVersion: 2020 });
+
+walk.simple(ast, {
+    FunctionDeclaration(node) {
+        const startTimeNode = acorn.parse('const startTime = performance.now();').body[0];
+        const endTimeNode = acorn.parse('const endTime = performance.now();').body[0];
+        const logNode = acorn.parse('console.log(`Function ${name} took ${endTime - startTime} ms`);').body[0];
+
+        node.body.body.unshift(startTimeNode);
+        node.body.body.push(endTimeNode, logNode);
+    }
+});
+
+const instrumentedCode = escodegen.generate(ast);
+
+console.log(instrumentedCode);
